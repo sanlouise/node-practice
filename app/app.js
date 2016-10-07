ecb6d17b7fb2ac86cb0d7e3ce86213f3dbc73b4f
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 19:11:33 -0600

    Create basic Node server

--- app/app.js
+++ app/app.js
@@ -2,4 +2,9 @@ var http = require('http');
 
 var myServer = http.createServer(function(req, res) {
 	res.writeHead(200, {"Content-Type" :"text/plain"});
+
+	res.write('Business Chat');
+	res.end();
 });
+
+myServer.listen(3000);
