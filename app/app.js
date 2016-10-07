12b7f9bc340e69030164f9d07af251985485e621
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 19:52:31 -0600

    Add route for individual artists

--- app/app.js
+++ app/app.js
@@ -5,6 +5,15 @@ var dataFile = require('./data/data.json')
 app.set( 'port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
+	res.send(`
+
+		<h1>Welcome</h1>
+		<p>Look for a meeting of your interest!</p>
+
+		`);
+});
+
+app.get('/speakers', function(req, res) {
 	var info = '';
 	dataFile.speakers.forEach(function(item){
 		info += `
@@ -22,18 +31,18 @@ app.get('/', function(req, res) {
 		`);
 });
 
-var server = app.listen(app.get('port'), function() {
-	console.log('Express is listening on localhost ' + app.get('port'))
+app.get('/speakers/:speakerid', function(req, res) {
+	var speaker = dataFile.speakers[req.params.speakerid];
+	res.send(`
+
+		<h1>${speaker.title}</h1>
+		<h3>with ${speaker.name}</h3>
+		<p>${speaker.summary}</p>
+
+		`);
 });
 
 
-// var http = require('http');
-
-// var myServer = http.createServer(function(req, res) {
-// 	res.writeHead(200, {"Content-Type" :"text/html"});
-// 	res.write('Business Chat');
-// 	res.end();
-// });
-
-// myServer.listen(3000);
-// console.log('Go to localhost 3000');
+var server = app.listen(app.get('port'), function() {
+	console.log('Express is listening on localhost ' + app.get('port'))
+});
