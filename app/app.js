6a37b77abd35f2e7cc29e9f194af5bd87908473e
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 19:13:28 -0600

    Update text to http

--- app/app.js
+++ app/app.js
@@ -1,8 +1,7 @@
 var http = require('http');
 
 var myServer = http.createServer(function(req, res) {
-	res.writeHead(200, {"Content-Type" :"text/plain"});
-
+	res.writeHead(200, {"Content-Type" :"text/html"});
 	res.write('Business Chat');
 	res.end();
 });
