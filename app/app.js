b7a90b9b9756792c1655ead7a3e994f0a38c4347
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 19:17:57 -0600

    Create Express server

--- app/app.js
+++ app/app.js
@@ -1,9 +1,21 @@
-var http = require('http');
-
-var myServer = http.createServer(function(req, res) {
-	res.writeHead(200, {"Content-Type" :"text/html"});
-	res.write('Business Chat');
-	res.end();
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+	res.send('<h1>Business Chat</h1>');
 });
 
-myServer.listen(3000);
+var server = app.listen(3000, function() {
+	console.log('Express is listening on localhost 3000')
+});
+
+
+// var http = require('http');
+
+// var myServer = http.createServer(function(req, res) {
+// 	res.writeHead(200, {"Content-Type" :"text/html"});
+// 	res.write('Business Chat');
+// 	res.end();
+// });
+
+// myServer.listen(3000);
+// console.log('Go to localhost 3000');
