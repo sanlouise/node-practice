584a7cd6793ebb93bb69fc106e2436330fd9e306
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 19:32:04 -0600

    Update flexible PORT

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json')
+
+app.set( 'port', process.env.PORT || 3000 );
+
 app.get('/', function(req, res) {
 	res.send('<h1>Business Chat</h1>');
 });
 
-var server = app.listen(3000, function() {
-	console.log('Express is listening on localhost 3000')
+var server = app.listen(app.get('port'), function() {
+	console.log('Express is listening on localhost ' + app.get('port'))
 });
 
 
