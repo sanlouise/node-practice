d00c5cc522852d35957a937615601d876a3b3ef6
sanlouise <sandra.adamshallie@gmail.com>
2016-10-06 21:04:29 -0600

    Include stylesheets

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json')
 
-app.set( 'port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 3000);
 //Set variable to be available to all routes in our app
 app.set('appData', dataFile);
 
+//Make css, images, js available to all routes
+app.use(express.static('app/public'));
 
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
